9acbdfaeefaa3ded385e20da499ebef99d1fe57e
shuvo-sarkaR-sys <md.sirforce@gmail.com>
2025-11-05 00:03:12 +0600

    upd

--- frontend/src/components/LatestNews.jsx
+++ frontend/src/components/LatestNews.jsx
@@ -32,44 +32,44 @@ const LatestNews = () => {
           <SwiperSlide >
             <img src={image1} alt="" />
             <div className='flex gap-2 items-center w-fit px-3 py-1 mt-4 rounded-md bg-[#516da566]'><div className='w-2 h-2 rounded-full bg-[#29509f]'></div> <p>Development</p></div>
-            <h1 className='text-2xl mt-3 font-bold text-gray-700'>HTML5- The Future of Web App Development</h1>
+            <h1 className='md:text-2xl text-lg line-clamp-2 mt-3 font-bold text-gray-700'>HTML5- The Future of Web App Development</h1>
             <div className='flex gap-5'><p>September 02, 2025</p> <p>4 mins read</p></div>
           </SwiperSlide>
           <SwiperSlide>
             <img src={image2} alt="" />
            <div className='flex gap-2 items-center w-fit px-3 py-1 mt-4 rounded-md bg-[#516da566]'><div className='w-2 h-2 rounded-full bg-[#29509f]'></div> <p>Technology</p></div>
-            <h1 className='text-2xl mt-3 font-bold text-gray-700'>The latest technologies to be used for VR in 2025</h1>
+            <h1 className='md:text-2xl text-lg line-clamp-2 mt-3 font-bold text-gray-700'>The latest technologies to be used for VR in 2025</h1>
             <div className='flex gap-5'><p>September 02, 2025</p> <p>4 mins read</p></div>
           </SwiperSlide>
           <SwiperSlide>
             <img src={image3} alt="" />
             <div className='flex gap-2 items-center w-fit px-3 py-1 mt-4 rounded-md bg-[#516da566]'><div className='w-2 h-2 rounded-full bg-[#29509f]'></div> <p>Gaming</p></div>
-            <h1 className='text-2xl mt-3 font-bold text-gray-700'>The Rise of Cloud Gaming in 2025</h1>
+            <h1 className='md:text-2xl text-lg line-clamp-2 mt-3 font-bold text-gray-700'>The Rise of Cloud Gaming in 2025</h1>
             <div className='flex gap-5'><p>September 02, 2025</p> <p>4 mins read</p></div>
 
           </SwiperSlide>
           <SwiperSlide>
             <img src={image4} alt="" />
             <div className='flex gap-2 items-center w-fit px-3 py-1 mt-4 rounded-md bg-[#516da566]'><div className='w-2 h-2 rounded-full bg-[#29509f]'></div> <p>Blockchain</p></div>
-            <h1 className='text-2xl mt-3 font-bold text-gray-700'>Exploring the Impact of Blockchain Technology</h1>
+            <h1 className='md:text-2xl text-lg line-clamp-2 mt-3 font-bold text-gray-700'>Exploring the Impact of Blockchain Technology</h1>
             <div className='flex gap-5'><p>September 02, 2025</p> <p>4 mins read</p></div>
           </SwiperSlide>
           <SwiperSlide>
             <img src={image5} alt="" />
             <div className='flex gap-2 items-center w-fit px-3 py-1 mt-4 rounded-md bg-[#516da566]'><div className='w-2 h-2 rounded-full bg-[#29509f]'></div> <p>Technology</p></div>
-            <h1 className='text-2xl mt-3 font-bold text-gray-700'>The latest technologies to be used for VR in 2025</h1>
+            <h1 className='md:text-2xl text-lg line-clamp-2 mt-3 font-bold text-gray-700'>The latest technologies to be used for VR in 2025</h1>
             <div className='flex gap-5'><p>September 02, 2025</p> <p>4 mins read</p></div>
           </SwiperSlide>
         </Swiper>
         <div className='grid grid-cols-2 md:grid-cols-2 xl:grid-cols-4 gap-6 mt-10 xl:mt-26 mb-8'>
-          <div className='flex gap-4 border-gray-200 border rounded-md px-2 py-3 h-20'> <img src={deliver} className='w-10'  alt="" /><div><p className='font-semibold text-xl'>Free Delivery</p><p className='text-gray-400'>From all orders over $10</p></div></div>
-          <div  className='flex gap-4 border-gray-200 border rounded-md px-2 py-3 h-20'> <img src={support} className='w-10'   alt="" /><div><p className='font-semibold text-xl'>24/7 Support</p><p className='text-gray-400'>Shop with an expert</p></div></div>
-          <div className='flex gap-4 border-gray-200 border rounded-md px-2 py-3 h-20'> <img src={secure} className='w-10' alt="" /><div><p className='font-semibold text-xl'>Secure Payments</p><p className='text-gray-400'>Shop with confidence</p></div></div>
-          <div className='flex gap-4 border-gray-200 border rounded-md px-2 py-3 h-20'> <img src={voucher} className='w-10' alt="" /><div><p className='font-semibold text-xl'>Discount Vouchers</p><p className='text-gray-400'>Get the best deals</p></div></div>
+          <div className='flex gap-4 border-gray-200 border rounded-md px-2 py-3 h-20'> <img src={deliver} className='w-10'  alt="" /><div><p className='font-semibold text-sm md:text-xl'>Free Delivery</p><p className='text-gray-400 md:text-sm text-xs'>From all orders over $10</p></div></div>
+          <div  className='flex gap-4 border-gray-200 border rounded-md px-2 py-3 h-20'> <img src={support} className='w-10'   alt="" /><div><p className='font-semibold text-sm md:text-xl'>24/7 Support</p><p className='text-gray-400 md:text-sm text-xs'>Shop with an expert</p></div></div>
+          <div className='flex gap-4 border-gray-200 border rounded-md px-2 py-3 h-20'> <img src={secure} className='w-10' alt="" /><div><p className='font-semibold text-sm md:text-xl'>Secure Payments</p><p className='text-gray-400 md:text-sm text-xs'>Shop with confidence</p></div></div>
+          <div className='flex gap-4 border-gray-200 border rounded-md px-2 py-3 h-20'> <img src={voucher} className='w-10' alt="" /><div><p className='font-semibold text-sm md:text-xl'>Discount Vouchers</p><p className='text-gray-400 md:text-sm text-xs'>Get the best deals</p></div></div>
          </div>
          <div className='bg-[#0E224D] p-4 h-56 box-4'>
-          <h1 className='text-4xl  mt-14 ml-10 font-bold text-white'>Subscrible & Get <span className='text-yellow-600'>10%</span> Discount</h1>
-           <p className='text-xl font-semibold text-white ml-10'>Get E-mail updates about our latest shop and special offers.</p>
+          <h1 className='md:text-4xl text-xl md:text-start text-center  md:mt-14 md:ml-10 font-bold text-white'>Subscrible & Get <span className='text-yellow-600'>10%</span> Discount</h1>
+           <p className='md:text-xl text-xs font-semibold text-white ml-10'>Get E-mail updates about our latest shop and special offers.</p>
           </div>
         </div>
   )
